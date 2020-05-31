2ece14e4f4497dbeeba44bb2489258b5a24e6488
jobi <olajobinurudeen2013@gmail.com>
2020-05-31 11:50:15 +0100

    modify package.json

---
+++ build/precache-manifest.20febfb41258e523fdd80b975ece9fd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7da638f81557cf27546551e0b9017cf8",
+    "url": "/face-recognition/index.html"
+  },
+  {
+    "revision": "dba25d1934a333d5271e",
+    "url": "/face-recognition/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "f883a4007f63e9c74c35",
+    "url": "/face-recognition/static/css/main.49529684.chunk.css"
+  },
+  {
+    "revision": "dba25d1934a333d5271e",
+    "url": "/face-recognition/static/js/2.641c765c.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/face-recognition/static/js/2.641c765c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f883a4007f63e9c74c35",
+    "url": "/face-recognition/static/js/main.bb53ed5c.chunk.js"
+  },
+  {
+    "revision": "fa1bc4860708b895953c",
+    "url": "/face-recognition/static/js/runtime-main.976500cc.js"
+  }
+]);
